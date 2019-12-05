d86fb062a0328734e1291e899b425593a6f0fd5b
Mason Bially <mason.bially@gmail.com>
2019-12-05 00:05:47 -0800

    Solving Day 02 B.

---
+++ day02/el_b.exs
@@ -0,0 +1,18 @@
+Code.require_file("el.ex", __DIR__)
+
+init_memory = Day02.read_intcode("day02/input.txt")
+
+try do
+  for noun <- 0..99,
+      verb <- 0..99 do
+    memory = :array.set(1, noun, init_memory)
+    memory = :array.set(2, verb, memory)
+
+    memory = Day02.run_program({memory, 0})
+
+    result = :array.get(0, memory)
+    if result == 19690720, do: throw({noun, verb})
+  end
+catch
+  {noun, verb} -> IO.inspect(100*noun + verb)
+end
