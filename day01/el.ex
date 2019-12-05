2c5c54b82501de2c6c2252f9cfbcbcdae23a2e74
Mason Bially <mason.bially@gmail.com>
2019-12-04 17:29:13 -0800

    Adding module import.

---
+++ day01/el.ex
@@ -0,0 +1,5 @@
+defmodule Day01 do
+  def fuel_equation(mass) do
+    floor(mass / 3) - 2
+  end
+end
--- day01/el_a.exs
+++ day01/el_a.exs
@@ -1,11 +1,7 @@
-defmodule Day01 do
-  def module_launch_fuel(mass) do
-    floor(mass / 3) - 2
-  end
-end
+Code.require_file("el.ex", __DIR__)
 
 File.stream!("day01/input.txt", [encoding: :utf8], :line)
 |> Stream.map(&elem(Integer.parse(&1), 0))
-|> Stream.map(&Day01.module_launch_fuel(&1))
+|> Stream.map(&Day01.fuel_equation(&1))
 |> Enum.sum
 |> IO.inspect
